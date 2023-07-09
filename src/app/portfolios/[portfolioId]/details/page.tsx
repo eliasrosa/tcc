391f8d3f8d9b67b0dbdb5bde24311af55aeb0d44
Elias da Rosa <elias@eliasdarosa.com.br>
2023-07-09 16:44:21 -0300

    bk

--- src/app/portfolios/[portfolioId]/details/page.tsx
+++ src/app/portfolios/[portfolioId]/details/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { Portfolio, PortfoliosContextType } from "@/@types/PortfoliosTypes"
+import { Ticker, TickersContextType } from "@/@types/TickersTypes"
+import { PageTitle } from "@/components/PageTitle"
+import { TableTickers } from "@/components/TableTickers"
+import { PortfoliosContext } from "@/context/PortfoliosProvider"
+import { TickersContext } from "@/context/TickersProvider"
+import { Card } from "@tremor/react"
+import { useContext, useEffect, useState } from "react"
 
 interface AnalisesProps {
   params: {
@@ -6,10 +16,35 @@ interface AnalisesProps {
 }
 
 export default function PagePortfolioDetails({ params }: AnalisesProps) {
+  const { getPortfolio } = useContext(PortfoliosContext) as PortfoliosContextType
+  const { listByPortfolioId } = useContext(TickersContext) as TickersContextType
+
+
+  const [portfolio, setPortfolio] = useState<Portfolio | false>(false)
+  const [tickers, setTickers] = useState<Ticker[] | false>(false)
+
+  useEffect(() => {
+    setPortfolio(getPortfolio(params.portfolioId))
+    setTickers(listByPortfolioId(params.portfolioId))
+  }, [])
+
+  if(!portfolio || !tickers){
+    return (
+      <PageTitle>Loading...</PageTitle>
+    )
+  }
 
   return (
     <div>
-      <h1>Page</h1>
+      <PageTitle>
+        <span>Análise da carteira</span>
+        <span>{portfolio.name}</span>
+      </PageTitle>
+
+      <Card className="p-2">
+        <TableTickers tickers={tickers} showButtons={true} />
+      </Card>
+
     </div>
   )
 }
