3c263100c8c92e16f6f2ae440861b1d5fb27452f
Elias da Rosa <elias@eliasdarosa.com.br>
2023-11-26 16:21:07 -0300

    refactor: change tickersInitialState

--- src/storage/tickersInitialState.ts
+++ src/storage/tickersInitialState.ts
@@ -1,10 +1,11 @@
 import { Ticker } from '@/@types/TickersTypes'
 import { config } from '@/config'
+import { portfoliosInitialState } from './portfoliosInitialState'
 
-const portfolioId = '5a31ee47-6b2f-4586-8fb9-983010149ec7'
+const portfolioId = portfoliosInitialState[0].id
 
 export const tickersInitialState: Ticker[] = [
   { ...config.defaults.ticker, portfolioId, ticker: 'XPLG11' },
   { ...config.defaults.ticker, portfolioId, ticker: 'XPML11' },
-  // { ...config.defaults.ticker, portfolioId, ticker: 'SARE11' },
+  { ...config.defaults.ticker, portfolioId, ticker: 'PVBI11' },
 ]
